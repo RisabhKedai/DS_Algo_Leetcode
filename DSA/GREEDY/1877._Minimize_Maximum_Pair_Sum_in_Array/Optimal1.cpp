ea773c0348ba62210be3b983751bd26bfe2b8f26
RisabhKedai <risabh.kedia12@gmail.com>
2024-01-04 21:55:35 +0530

    1877. Minimize Maximum Pair Sum in Array

---
+++ DSA/GREEDY/1877._Minimize_Maximum_Pair_Sum_in_Array/Optimal1.cpp
@@ -0,0 +1,27 @@
+/*
+* @Author: lenovo
+* @Date:   2024-01-04 21:35:27
+* @Last Modified by:   lenovo
+* @Last Modified time: 2024-01-04 21:36:25
+*/
+class Solution {
+public:
+    int minPairSum(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int n = nums.size();
+        int ans = -1;
+        int i = 0; 
+        int j = n-1;
+        while(i<j) {
+            ans = max(nums[i++]+nums[j--],ans);
+        }
+        return ans;
+    }
+};
+
+/**
+ * Sort the array and then pair maximum element with minimum one
+ *
+ * Time complexity: O(N)
+ * Space complexity : O(1)
+ */
