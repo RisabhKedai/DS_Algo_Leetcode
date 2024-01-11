9b07e8fcc6dd34f356a70578e851265c447a2dfd
RisabhKedai <risabh.kedia12@gmail.com>
2024-01-11 22:32:04 +0530

    1366. Rank Teams by Votes

---
+++ DSA/MAP/1366._Rank_Teams_by_Votes/Optimal1.cpp
@@ -0,0 +1,39 @@
+/*
+* @Author: lenovo
+* @Date:   2024-01-11 22:25:32
+* @Last Modified by:   lenovo
+* @Last Modified time: 2024-01-11 22:30:43
+*/
+
+
+class Solution {
+public:
+    string rankTeams(vector<string>& votes) {
+        vector<vector<int>> table(26,vector<int>(votes[0].length()+1,0));
+        for(int i=0; i<26; i++){
+            table[i][votes[0].length()] = i;
+        }
+        for(auto vote : votes) {
+            for(int i=0; i<vote.length(); i++) {
+                table[vote[i]-'A'][i]--;
+            }
+        }
+        sort(table.begin(), table.end());
+
+        string ans = "";
+        for(int i=0; i<26; i++) {
+            ans += (char)('A' + table[i][votes[0].length()]);
+        }
+        
+        return ans.substr(0,votes[0].length());
+    }
+};
+
+/**
+ * For each letter have the frequency of votes they have for each position. 
+ * Sort individial letters according to the data 
+ * form a sequence accoring to this table. 
+ * 
+ * Time complexity : O(N * size of each string)
+ * Space complexity : O(26 * size of each string)
+ */
